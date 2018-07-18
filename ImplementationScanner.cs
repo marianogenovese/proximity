8f5a8f68f42bcc9d1c6c698e005b28e75d563bf8
Mariano <mgenovese@iid.ec>
2018-07-18 15:47:11 -0500

    DOCs

--- ImplementationScanner.cs
+++ ImplementationScanner.cs
@@ -1,3 +1,6 @@
+// Esta clase por debajo usa BT para detectar cualquier objecto (beacon) sin importar
+// que este relacionado o no, basicamente necesitamos que esta clase inicie todo el proceso de 
+// deteccion de objetos y cambios.
 public class ImplementationScanner : EstimoteProximityScanner
 {
     public ImplementationScanner(ProximityObserver estimoteProximityObserver, string tag, double distance) : base(estimoteProximityObserver, tag, Guid.Empty, distance)
