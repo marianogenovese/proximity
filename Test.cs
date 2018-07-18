b195e3c5d115750f9b68464c85ce74fb7c2e3a38
Mariano <mgenovese@iid.ec>
2018-07-18 15:50:50 -0500

    DOCs

--- Test.cs
+++ Test.cs
@@ -3,6 +3,8 @@ ProximityObserver pObserver = new ... (); // estimote observer
 Implementation currentImplementation = null;
 SectorScanner sectorScanner = null;
 
+// ImplementationScanner =  ahora esta clase va a decir oye tal vez detecte una implementación porque 
+// detecte un beacon, entonces se debe preguntar se detecto un beacon de la misma implementacion o de otra??
 ImplementationScanner implementationScanner = new ImplementationScanner(pObserver, "DefaultAuparZone", 50);
 implementationScanner.throttle(4).Subscribe((beaconDetected) => {
     //Detecto un beacon de alguna implementación, debo calcular si es la misma impl o es otra
@@ -10,14 +12,18 @@ implementationScanner.throttle(4).Subscribe((beaconDetected) => {
     IImplementationProvider implProvider = ImplementationProvider.Get();
     Implementation implementation = implProvider.FindByBeaconId(beaconDetected.Id);
     
+    //Si es la primera vez, o si son 2 implementaciones distintas.
     if(currentImplementation == null || currentImplementation.Id != implementation.Id)
-    {  
+    {
         currentImplementation = implementation;
+        // Libero los recursos asociado al escaneo de sectores
         if (sectorScanner != null)
         {
             sectorScanner.Dispose();
         }
 
+        // SectorScanner = ahora esta clase recibe una implementación para poder detectar todos
+        // los sectores asociados a la implementación y poder recibir noficaciones.
         sectorScanner = new SectorScanner(pObserver, implementation);
         sectorScanner.throttle(0.5).Subscribe((sectorDetected) => {
             //Se detecto un sector
